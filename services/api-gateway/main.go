bf827b2ed361d8a86ca26f06657e9cf1716903c8
rzarviv@gmail.com <rzarviv@gmail.com>
2023-11-15 09:20:39 +0200

    initial commit

---
+++ services/api-gateway/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("hi there")
+	initGateway()
+}
+
+func initGateway() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	port := 9000
+	if err := router.Run(fmt.Sprintf(":%d", port)); err != nil { 
+		log.Fatalf("cannot start api gateway on port %d, error is :%s", port, err.Error())
+	} 
+}
